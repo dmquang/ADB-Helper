1e5afcca21c228f66b79bcc4f6468fd72dd2976f
Rudyy Greyrat <89626745+dmquang@users.noreply.github.com>
2024-09-23 20:06:16 +0700

    Delete test.py

--- test.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-
-a = subprocess.run(r'adb push "C:\Users\Rudyy\Documents\instagram\Reg Phone\ADB-Helper\images" /sdcard/Pictures/',text=True, stdout=subprocess.PIPE,)
-print(a.stdout)
